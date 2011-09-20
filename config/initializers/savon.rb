a025d212c943e5749875e335c73117f60bc061d5
Tom Taylor <tom@tomtaylor.co.uk>
2011-09-20 10:45:57 +0100

    Ensure Savon only logs in development/test.

--- config/initializers/savon.rb
+++ config/initializers/savon.rb
@@ -1,5 +1,5 @@
 Savon.configure do |config|
-  # config.log = false            # disable logging
-  config.log_level = :info      # changing the log level
+  config.log = true
+  config.log_level = :debug     # changing the log level
   config.logger = Rails.logger  # using the Rails logger
 end
