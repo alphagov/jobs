00de2b1810839e82736f35d8a96f0d148a6b34f1
Tom Taylor <tom@tomtaylor.co.uk>
2011-09-20 15:14:25 +0100

    Testing import_details_from_hash.

--- test/unit/vacancy_test.rb
+++ test/unit/vacancy_test.rb
@@ -95,4 +95,28 @@ class VacancyTest < ActiveSupport::TestCase
     assert_equal 100, response.length
   end
 
+  test '.import_details_from_hash' do
+    vacancy_hash = {:currency=>"GBP", :date_received=>"30082011", :distance_sort_order_id=>"2396.97008934328", :es_vacancy=>"Y", :hours=>"35", :hours_display_text=>"37.5 HOURS OVER 5 DAYS", :hours_qualifier=>"per week", :is_national=>false, :is_regional=>false, :location=>{:distance_from_origin=>"2396.97008934328", :latitude=>"50.986008297409", :longitude=>"0.9740371746526", :origin_latitude=>"51", :origin_longitude=>"1", :location_name=>"NEW ROMNEY, KENT"}, :location_display_text=>"NEW ROMNEY, KENT", :order_id=>"1", :perm_temp=>"P", :quality=>"86", :received_on=> Time.utc(2011, 8, 30, 0, 0, 0), :soc_code=>"3543", :vacancy_detail=>{:hours=>"0", :soc_code=>"0"}, :vacancy_id=>"FOK/12116", :vacancy_title=>"CHARITY FUNDRAISER", :wage=>"See details", :wage_display_text=>"£255 TO £1000 PER WEEK", :wage_qualifier=>"NK", :wage_sort_order_id=>"20"}
+
+    vacancy = Vacancy.new
+    vacancy.expects(:vacancy_title=).with(vacancy_hash[:vacancy_title])
+    vacancy.expects(:received_on=).with(vacancy_hash[:received_on])
+    vacancy.expects(:soc_code=).with(vacancy_hash[:soc_code])
+    vacancy.expects(:wage=).with(vacancy_hash[:wage])
+    vacancy.expects(:wage_qualifier=).with(vacancy_hash[:wage_qualifier])
+    vacancy.expects(:wage_display_text=).with(vacancy_hash[:wage_display_text])
+    vacancy.expects(:currency=).with(vacancy_hash[:currency])
+    vacancy.expects(:is_national=).with(vacancy_hash[:is_national])
+    vacancy.expects(:is_regional=).with(vacancy_hash[:is_regional])
+    vacancy.expects(:hours=).with(vacancy_hash[:hours].to_i)
+    vacancy.expects(:hours_qualifier=).with(vacancy_hash[:hours_qualifier])
+    vacancy.expects(:hours_display_text=).with(vacancy_hash[:hours_display_text])
+    vacancy.expects(:location_name=).with(vacancy_hash[:location][:location_name])
+    vacancy.expects(:latitude=).with(vacancy_hash[:location][:latitude].to_f)
+    vacancy.expects(:longitude=).with(vacancy_hash[:location][:longitude].to_f)
+    vacancy.expects(:is_permanent=).with(true)
+
+    vacancy.import_details_from_hash(vacancy_hash)
+  end
+
 end
