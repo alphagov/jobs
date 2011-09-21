0f13f39374847a560d689f7e98ce17e0c3871204
Tom Taylor <tom@tomtaylor.co.uk>
2011-09-21 15:51:32 +0100

    Fixing the auto_link initializer. Oops.

--- config/initializers/auto_link.rb
+++ config/initializers/auto_link.rb
@@ -1,4 +1,2 @@
 # This is so we can use rel='nofollow' in auto_link.
-Jobs::Application.config.after_initialize do |config|
-  ActionView::Base.sanitized_allowed_attributes = 'rel'
-end
+Jobs::Application.config.action_view.sanitized_allowed_attributes = 're'
