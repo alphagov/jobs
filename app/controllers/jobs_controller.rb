e3e1f87cb10d86acd96bfa3d22432e73aa57ac57
Tom Taylor <tom@tomtaylor.co.uk>
2011-09-22 16:10:00 +0100

    Redirect to not found page for jobs if the job doesn't exist.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,6 +2,7 @@ class JobsController < ApplicationController
 
   def show
     @job = VacancySearch.find_individual(params[:id])
+    render :action => 'not_found', :status => :not_found if @job.nil?
   end
 
 end
