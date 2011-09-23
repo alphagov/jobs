1c93145687c6d18ae21073e6dc27f677f2386e4f
James Stewart <jys@ketlai.co.uk>
2011-09-23 11:33:22 +0100

    Add jenkins.sh so we can integrate into CI

---
+++ jenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+source '/usr/local/lib/rvm'
+bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:setup
+bundle exec rake db:migrate
+bundle exec rake stats
+
+# DELETE STATIC SYMLINKS AND RECONNECT...
+rm /var/lib/jenkins/jobs/Jobs/workspace/public/images
+rm /var/lib/jenkins/jobs/Jobs/workspace/public/javascripts
+rm /var/lib/jenkins/jobs/Jobs/workspace/public/templates
+rm /var/lib/jenkins/jobs/Jobs/workspace/public/stylesheets
+
+ln -s /var/lib/jenkins/jobs/Static/workspace/public/images /var/lib/jenkins/jobs/Jobs/workspace/public/images
+ln -s /var/lib/jenkins/jobs/Static/workspace/public/javascripts /var/lib/jenkins/jobs/Jobs/workspace/public/javascripts
+ln -s /var/lib/jenkins/jobs/Static/workspace/public/templates /var/lib/jenkins/jobs/Jobs/workspace/public/templates
+ln -s /var/lib/jenkins/jobs/Static/workspace/public/stylesheets /var/lib/jenkins/jobs/Jobs/workspace/public/stylesheets
+
+export DISPLAY=:99
+/etc/init.d/xvfb start
+bundle exec rake ci:setup:testunit test:units test:functionals test:integration
+RESULT=$?
+/etc/init.d/xvfb stop
+exit $RESULT
