cd846971c8f929968d602f0fb798f0e03e74ea93
James Stewart <jys@ketlai.co.uk>
2011-11-17 08:58:59 +0000

    All job routes start /job-search

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Jobs::Application.routes.draw do
-  root :to => 'home#show', :via => :get
-  match 'search', :to => 'search#show', :via => :get
-  match 'search', :to => 'search#show_post', :via => :post
-  match 'jobs/*id', :to => 'jobs#show', :via => :get, :as => :job
-  match 'sitemap.xml',
-    :to => 'sitemaps#index',
-    :via => :get,
-    :as => :sitemap_index
+  scope "job-search" do
+    root :to => 'home#show', :via => :get
+    match 'search', :to => 'search#show', :via => :get
+    match 'search', :to => 'search#show_post', :via => :post
+    match 'jobs/*id', :to => 'jobs#show', :via => :get, :as => :job
+    match 'sitemap.xml',
+      :to => 'sitemaps#index',
+      :via => :get,
+      :as => :sitemap_index
 
-  match 'sitemaps/:date.xml',
-    :to => 'sitemaps#show',
-    :via => :get,
-    :as => :sitemap,
-    :date => /\d{4}-\d{2}-\d{2}/
+    match 'sitemaps/:date.xml',
+      :to => 'sitemaps#show',
+      :via => :get,
+      :as => :sitemap,
+      :date => /\d{4}-\d{2}-\d{2}/
 
-  match 'print', :to => 'home#print', :via => :get
+    match 'print', :to => 'home#print', :via => :get
+  end
 end
